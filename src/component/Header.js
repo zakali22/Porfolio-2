dc89497d3daa00305bf0e42152f1cf4805afdd3f
Zakeria Ali <zak.fined@gmail.com>
2018-10-22 13:32:08 +0100

    Restructure project

---
+++ src/component/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = props => {
+  return (
+    <div className="Header">
+      <h2>za</h2>
+    </div>
+  );
+};
+
+export default Header;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./component/App";
+import { ParallaxProvider } from "react-scroll-parallax";
 
-
-ReactDOM.render(<App />, document.getElementById('root'));
-
+ReactDOM.render(<App />, document.getElementById("root"));
