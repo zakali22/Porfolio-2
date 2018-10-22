7bad3da0c5615b569bd1f36c2270d033a4e773fe
Zakeria Ali <zak.fined@gmail.com>
2018-10-22 15:40:07 +0100

    Add navigation

--- src/component/Header.js
+++ src/component/Header.js
@@ -2,8 +2,15 @@ import React from "react";
 
 const Header = props => {
   return (
-    <div className="Header">
-      <h2>za</h2>
+    <div className="header">
+      <div className="header__navigation">
+        <h2>za</h2>
+        <nav className="header__navigation--nav">
+          <p className="header__navigation--nav_item">about</p>
+          <p className="header__navigation--nav_item">portfolio</p>
+          <p className="header__navigation--nav_item">resume</p>
+        </nav>
+      </div>
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "./sass/index.scss";
 import App from "./component/App";
-import { ParallaxProvider } from "react-scroll-parallax";
 
 ReactDOM.render(<App />, document.getElementById("root"));
