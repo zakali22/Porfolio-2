7984d106ee72b050a6ea660266f236213b219392
Zakeria Ali <zak.fined@gmail.com>
2018-10-22 18:46:55 +0100

    Add headline layout and design

--- src/component/App.js
+++ src/component/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Header from "./Header";
 import Jumbotron from "./Jumbotron";
+import Headline from "./Headline/Headline";
 
 class App extends Component {
   render() {
@@ -8,6 +9,7 @@ class App extends Component {
       <div className="App">
         <Header />
         <Jumbotron />
+        <Headline />
       </div>
     );
   }
---
+++ src/component/Headline/Headline.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import Intro from "./Intro";
+
+class Headline extends Component {
+  render() {
+    return (
+      <div className="headline">
+        <Intro />
+      </div>
+    );
+  }
+}
+
+export default Headline;
